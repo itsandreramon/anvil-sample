f44a003ff05f2ce852a8102328c3b5be524b47f0
itsandreramon <thielea@th-brandenburg.de>
2024-01-01 13:36:50 +0100

    remove unused code

--- app/src/main/java/com/example/anvil/di/types.kt
+++ app/src/main/java/com/example/anvil/di/types.kt
@@ -1,9 +1,5 @@
-@file:Suppress("unused")
-
 package com.example.anvil.di
 
 typealias DaggerSet<T> = @JvmSuppressWildcards Set<T>
-
-typealias DaggerMap<K, V> = @JvmSuppressWildcards Map<K,V>
 
 typealias InitializerFunction = () -> @JvmSuppressWildcards Unit
